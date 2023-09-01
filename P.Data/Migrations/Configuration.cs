081e7fec47dbdffed2b1b00b51bf5cc0ae3d4f58
Savoiej <saavoie@gmail.com>
2023-09-01 07:29:39 -0400

    enabled migrations.

---
+++ P.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace P.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<P.Data.SQLContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(P.Data.SQLContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
